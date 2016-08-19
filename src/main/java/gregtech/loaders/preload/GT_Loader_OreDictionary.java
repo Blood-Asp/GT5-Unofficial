73254945781a6c5313f5e0ce109cd2df79c1bb05
Dream-Master <dream-master@gmx.net>
2016-08-19 21:23:47 +0200

    next try register all stuff

--- src/main/java/gregtech/loaders/preload/GT_Loader_OreDictionary.java
+++ src/main/java/gregtech/loaders/preload/GT_Loader_OreDictionary.java
@@ -203,8 +203,8 @@ public class GT_Loader_OreDictionary
         GT_OreDictUnificator.registerOre(OrePrefixes.circuit, Materials.Advanced, GT_ModHandler.getIC2Item("advancedCircuit", 1L));
 
         //Custom Stuff GT New Horizons 1.0.9.2
-        GT_OreDictUnificator.registerOre(OrePrefixes.ingot, Materials.BloodInfusedIron, GT_ModHandler.getModItem("BloodArsenal", "blood_infused_iron", 1L, 0));
-        GT_OreDictUnificator.registerOre(OrePrefixes.block, Materials.BloodInfusedIron, GT_ModHandler.getModItem("BloodArsenal", "blood_infused_iron_block", 1L, 0));
+        GT_OreDictUnificator.registerOre("ingotBloodInfusedIron", GT_ModHandler.getModItem("BloodArsenal", "blood_infused_iron", 1L, 0));
+        GT_OreDictUnificator.registerOre("blockBloodInfusedIron", GT_ModHandler.getModItem("BloodArsenal", "blood_infused_iron_block", 1L, 0));
 
         GT_OreDictUnificator.registerOre(OrePrefixes.log, Materials.Wood, GT_ModHandler.getModItem("BiomesOPlenty", "logs4", 1L, 3));
 
@@ -216,28 +216,28 @@ public class GT_Loader_OreDictionary
         GT_OreDictUnificator.registerOre(OrePrefixes.block, Materials.Mytryl, GT_ModHandler.getModItem("dreamcraft", "tile.Mytryl", 1L, 0));
         GT_OreDictUnificator.registerOre(OrePrefixes.ingot, Materials.CallistoIce, com.dreammaster.item.ItemList.CallistoIceIngot.getIS());
         GT_OreDictUnificator.registerOre("ingotColdCallistoIce", com.dreammaster.item.ItemList.CallistoIceColdIngot.getIS());
-        GT_OreDictUnificator.registerOre(OrePrefixes.block, Materials.CallistoIce, GT_ModHandler.getModItem("dreamcraft", "tile.CallistoColdIce", 1L, 0));
+        GT_OreDictUnificator.registerOre("blockCallistoIce", GT_ModHandler.getModItem("dreamcraft", "tile.CallistoColdIce", 1L, 0));
         GT_OreDictUnificator.registerOre(OrePrefixes.dust, Materials.CallistoIce, com.dreammaster.item.ItemList.CallistoIceDust.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.plate, Materials.CallistoIce, com.dreammaster.item.ItemList.CallistoIcePlate.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.plateDense, Materials.CallistoIce, com.dreammaster.item.ItemList.CallistoIceDensePlate.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.compressed, Materials.CallistoIce, com.dreammaster.item.ItemList.CallistoIceCompressedPlate.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.ingot, Materials.Ledox, com.dreammaster.item.ItemList.LedoxIngot.getIS());
         GT_OreDictUnificator.registerOre("ingotColdLedox", com.dreammaster.item.ItemList.LedoxColdIngot.getIS());
-        GT_OreDictUnificator.registerOre(OrePrefixes.block, Materials.Ledox,  GT_ModHandler.getModItem("dreamcraft", "tile.Ledox", 1L, 0));
+        GT_OreDictUnificator.registerOre("blockLedox",  GT_ModHandler.getModItem("dreamcraft", "tile.Ledox", 1L, 0));
         GT_OreDictUnificator.registerOre(OrePrefixes.dust, Materials.Ledox, com.dreammaster.item.ItemList.LedoxDust.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.plate, Materials.Ledox, com.dreammaster.item.ItemList.LedoxPlate.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.plateDense, Materials.Ledox, com.dreammaster.item.ItemList.LedoxDensePlate.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.compressed, Materials.Ledox, com.dreammaster.item.ItemList.LedoxCompressedPlate.getIS());
-        GT_OreDictUnificator.registerOre(OrePrefixes.rod, Materials.Stone, com.dreammaster.item.ItemList.CobbleStoneRod.getIS());
+        GT_OreDictUnificator.registerOre(OrePrefixes.stick, Materials.Stone, com.dreammaster.item.ItemList.CobbleStoneRod.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.plate, Materials.Stone, com.dreammaster.item.ItemList.StonePlate.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.plate, Materials.Voidmetal, com.dreammaster.item.ItemList.VoidPlate.getIS());
-        GT_OreDictUnificator.registerOre(OrePrefixes.rod, Materials.Voidmetal, com.dreammaster.item.ItemList.VoidRod.getIS());
+        GT_OreDictUnificator.registerOre(OrePrefixes.stick, Materials.Voidmetal, com.dreammaster.item.ItemList.VoidRod.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.ring, Materials.Voidmetal, com.dreammaster.item.ItemList.VoidRing.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.foil, Materials.Voidmetal, com.dreammaster.item.ItemList.VoidFoil.getIS());
         GT_OreDictUnificator.registerOre("stickSandstone", com.dreammaster.item.ItemList.SandStoneRod.getIS());
         GT_OreDictUnificator.registerOre("lensReinforcedGlass", com.dreammaster.item.ItemList.ReinforcedGlassLense.getIS());
         GT_OreDictUnificator.registerOre("plateReinforcedGlass", com.dreammaster.item.ItemList.ReinforcedGlassPlate.getIS());
-        GT_OreDictUnificator.registerOre(OrePrefixes.block, Materials.Quantium, GT_ModHandler.getModItem("dreamcraft", "tile.Quantinum", 1L, 0));
+        GT_OreDictUnificator.registerOre("blockQuantinum", GT_ModHandler.getModItem("dreamcraft", "tile.Quantinum", 1L, 0));
         GT_OreDictUnificator.registerOre(OrePrefixes.compressed, Materials.Quantium, com.dreammaster.item.ItemList.QuantinumCompressedPlate.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.dust, Materials.Quantium, com.dreammaster.item.ItemList.QuantinumDust.getIS());
         GT_OreDictUnificator.registerOre(OrePrefixes.plateDense, Materials.Quantium, com.dreammaster.item.ItemList.QuantinumDensePlate.getIS());
@@ -255,20 +255,19 @@ public class GT_Loader_OreDictionary
         GT_OreDictUnificator.registerOre(OrePrefixes.stick, Materials.Desh, GT_ModHandler.getModItem("GalacticraftMars", "item.null", 1L, 1));
         GT_OreDictUnificator.registerOre(OrePrefixes.ore, Materials.Ilmenite, GT_ModHandler.getModItem("GalacticraftMars", "tile.asteroidsBlock", 1L, 4));
 
-        GT_OreDictUnificator.registerOre(OrePrefixes.ore,  Materials.Adamantium, GT_ModHandler.getModItem("GalaxySpace", "oberonblocks", 1L, 3));
-        GT_OreDictUnificator.registerOre(OrePrefixes.ingot,  Materials.Adamantium, GT_ModHandler.getModItem("GalaxySpace", "items.ingots", 1L, 0));
-        GT_OreDictUnificator.registerOre(OrePrefixes.ore,  Materials.Cobalt, GT_ModHandler.getModItem("GalaxySpace", "phobosblocks", 1L, 4));
-        GT_OreDictUnificator.registerOre(OrePrefixes.ingot,  Materials.Cobalt, GT_ModHandler.getModItem("GalaxySpace", "items.ingots", 1L, 1));
+        GT_OreDictUnificator.registerOre("oreAdamantium", GT_ModHandler.getModItem("GalaxySpace", "oberonblocks", 1L, 3));
+        GT_OreDictUnificator.registerOre(OrePrefixes.ingot,  Materials.Adamantium, GT_ModHandler.getModItem("GalaxySpace", "items.Ingots", 1L, 0));
+        GT_OreDictUnificator.registerOre("oreCobalt", GT_ModHandler.getModItem("GalaxySpace", "phobosblocks", 1L, 4));
+        GT_OreDictUnificator.registerOre(OrePrefixes.ingot,  Materials.Cobalt, GT_ModHandler.getModItem("GalaxySpace", "items.Ingots", 1L, 1));
 
         GT_OreDictUnificator.registerOre("craftingToolShears", new ItemStack(Items.shears, 1, 32767));
 
         GT_OreDictUnificator.registerOre("cropBarley", GT_ModHandler.getModItem("Natura", "barleyFood", 1L, 0));
-        GT_OreDictUnificator.registerOre("cropBarley", GT_ModHandler.getModItem("Natura", "barleyFood", 1L, 0));
 
-        GT_OreDictUnificator.registerOre("craftingToolShears", GT_ModHandler.getModItem("ExtraUtilities", "shears", 1L, 0));
+        GT_OreDictUnificator.registerOre("craftingToolShears", GT_ModHandler.getModItem("ExtraUtilities", "shears", 1L, 32767));
 
-        GT_OreDictUnificator.registerOre(OrePrefixes.log, Materials.Wood, GT_ModHandler.getModItem("ForbiddenMagic", "TaintLog", 1L, 0));
-        GT_OreDictUnificator.registerOre(OrePrefixes.plank, Materials.Wood, GT_ModHandler.getModItem("ForbiddenMagic", "TaintPlank", 1L, 0));
+        GT_OreDictUnificator.registerOre("logWood", GT_ModHandler.getModItem("ForbiddenMagic", "TaintLog", 1L, 0));
+        GT_OreDictUnificator.registerOre("plankWood", GT_ModHandler.getModItem("ForbiddenMagic", "TaintPlank", 1L, 0));
 
         GT_OreDictUnificator.registerOre("beeComb", GT_ModHandler.getModItem("gendustry", "HoneyComb", 1L));
 
