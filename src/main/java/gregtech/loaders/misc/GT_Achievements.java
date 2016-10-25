a70e237cf82bac7ef7bb9cdbe5d5509d7a1bcc4a
Technus <daniel112092@gmail.com>
2016-10-25 12:01:56 +0200

    Fix, slabs no longer given on paper crafting , since we dont use that recipe

--- src/main/java/gregtech/loaders/misc/GT_Achievements.java
+++ src/main/java/gregtech/loaders/misc/GT_Achievements.java
@@ -340,7 +340,7 @@ public class GT_Achievements {
         if (player == null || stack == null) {
             return;
         }
-        if(stack.getItem()==Items.paper){player.inventory.addItemStackToInventory(new ItemStack(Blocks.stone_slab,2));}
+        //if(stack.getItem()==Items.paper){player.inventory.addItemStackToInventory(new ItemStack(Blocks.stone_slab,2));}
         ItemData data = GT_OreDictUnificator.getItemData(stack);
         if (data != null) {
             if (data.mPrefix == OrePrefixes.dust && data.mMaterial.mMaterial == Materials.Bronze) {
