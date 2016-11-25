e15eb15fc2ae59a155ec34c70762d753a9ef8d26
Technus <daniel112092@gmail.com>
2016-11-25 15:48:35 +0100

    Added oreprocessing for small screws out of rods.

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingStick.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingStick.java
@@ -16,6 +16,7 @@ public class ProcessingStick implements gregtech.api.interfaces.IOreRecipeRegist
         if (!aMaterial.contains(gregtech.api.enums.SubTag.NO_WORKING))
             GT_Values.RA.addCutterRecipe(GT_Utility.copyAmount(1L, new Object[]{aStack}), GT_OreDictUnificator.get(OrePrefixes.bolt, aMaterial, 4L), null, (int) Math.max(aMaterial.getMass() * 2L, 1L), 4);
         if (!aMaterial.contains(gregtech.api.enums.SubTag.NO_SMASHING)) {
+            GT_Values.RA.addBenderRecipe(GT_Utility.copyAmount(1L, new Object[]{aStack}), GT_OreDictUnificator.get(OrePrefixes.springSmall, aMaterial, 2L), 100, 8);
             GT_Values.RA.addForgeHammerRecipe(GT_Utility.copyAmount(2L, new Object[]{aStack}), GT_OreDictUnificator.get(OrePrefixes.stickLong, aMaterial, 1L), (int) Math.max(aMaterial.getMass(), 1L), 16);
             GT_Values.RA.addWiremillRecipe(GT_Utility.copyAmount(1L, new Object[]{aStack}), GT_Utility.copy(new Object[]{GT_OreDictUnificator.get(OrePrefixes.wireGt01, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.wireFine, aMaterial, 4L)}), 50, 4);
         }
