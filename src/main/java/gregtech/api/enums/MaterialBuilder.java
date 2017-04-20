72c55d5726940b1b8746b472ebb84c73fe6391a8
Johannes Gäßler <updrn@student.kit.edu>
2017-04-20 09:28:26 +0200

    Refactored MaterialAdapter to MaterialBuilder to better represent the class functionality.

--- src/main/java/gregtech/api/enums/MaterialBuilder.java
+++ src/main/java/gregtech/api/enums/MaterialBuilder.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import gregtech.api.objects.MaterialStack;
 
-public class MaterialAdapter {
+public class MaterialBuilder {
 	public static final int DIESEL = 0, GAS = 1, THERMAL = 2, SEMIFLUID = 3, PLASMA = 4, MAGIC = 5;
 	
 	private int metaItemSubID;
@@ -32,7 +32,7 @@ public class MaterialAdapter {
 	private List<MaterialStack> materialList = new ArrayList<MaterialStack>();
 	private List<TC_Aspects.TC_AspectStack> aspects = new ArrayList<TC_Aspects.TC_AspectStack>();
 
-	public MaterialAdapter(int metaItemSubID, TextureSet iconSet, String defaultLocalName) {
+	public MaterialBuilder(int metaItemSubID, TextureSet iconSet, String defaultLocalName) {
 		this.metaItemSubID = metaItemSubID;
 		this.iconSet = iconSet;
 		this.name = defaultLocalName.replace(" ", "");
@@ -44,68 +44,68 @@ public class MaterialAdapter {
 				blastFurnaceRequired, transparent, oreValue, densityMultiplier, densityDivider, color, extraData, materialList, aspects);
 	}
 	
-	public MaterialAdapter setName(String name){
+	public MaterialBuilder setName(String name){
 		this.name = name;
 		return this;
 	}
 	
-	public MaterialAdapter setTypes(int types){
+	public MaterialBuilder setTypes(int types){
 		this.types = types;
 		return this;
 	}
 	
-	public MaterialAdapter addDustItems(){
+	public MaterialBuilder addDustItems(){
 		types = types | 1;
 		return this;
 	}
 	
-	public MaterialAdapter addMetalItems(){
+	public MaterialBuilder addMetalItems(){
 		types = types | 2;
 		return this;
 	}
 	
-	public MaterialAdapter addGemItems(){
+	public MaterialBuilder addGemItems(){
 		types = types | 4;
 		return this;
 	}
 	
-	public MaterialAdapter addOreItems(){
+	public MaterialBuilder addOreItems(){
 		types = types | 8;
 		return this;
 	}
 	
-	public MaterialAdapter addCell(){
+	public MaterialBuilder addCell(){
 		types = types | 16;
 		return this;
 	}
 	
-	public MaterialAdapter addPlasma(){
+	public MaterialBuilder addPlasma(){
 		types = types | 32;
 		return this;
 	}
 	
-	public MaterialAdapter addToolHeadItems(){
+	public MaterialBuilder addToolHeadItems(){
 		types = types | 64;
 		return this;
 	}
 	
-	public MaterialAdapter addGearItems(){
+	public MaterialBuilder addGearItems(){
 		types = types | 128;
 		return this;
 	}
 	
-	public MaterialAdapter addFluid(){
+	public MaterialBuilder addFluid(){
 		types = types | 256;
 		return this;
 	}
 	
-	public MaterialAdapter addGas(){
+	public MaterialBuilder addGas(){
 		types = types | 512;
 		return this;
 	}
 	
 	
-	public MaterialAdapter setRGBA(int r, int g, int b, int a){
+	public MaterialBuilder setRGBA(int r, int g, int b, int a){
 		this.r = r;
 		this.g = g;
 		this.b = b;
@@ -113,106 +113,106 @@ public class MaterialAdapter {
 		return this;
 	}
 	
-	public MaterialAdapter setRGB(int r, int g, int b){
+	public MaterialBuilder setRGB(int r, int g, int b){
 		this.r = r;
 		this.g = g;
 		this.b = b;
 		return this;
 	}
 	
-	public MaterialAdapter setTransparent(boolean transparent){
+	public MaterialBuilder setTransparent(boolean transparent){
 		this.transparent = transparent;
 		return this;
 	}
 	
-	public MaterialAdapter setColor(Dyes color){
+	public MaterialBuilder setColor(Dyes color){
 		this.color = color;
 		return this;
 	}
 
 
-	public MaterialAdapter setToolSpeed(float toolSpeed) {
+	public MaterialBuilder setToolSpeed(float toolSpeed) {
 		this.toolSpeed = toolSpeed;
 		return this;
 	}
 
-	public MaterialAdapter setDurability(int durability) {
+	public MaterialBuilder setDurability(int durability) {
 		this.durability = durability;
 		return this;
 	}
 
-	public MaterialAdapter setToolQuality(int toolQuality) {
+	public MaterialBuilder setToolQuality(int toolQuality) {
 		this.toolQuality = toolQuality;
 		return this;
 	}
 
 
-	public MaterialAdapter setFuelType(int fuelType) {
+	public MaterialBuilder setFuelType(int fuelType) {
 		this.fuelType = fuelType;
 		return this;
 	}
 
-	public MaterialAdapter setFuelPower(int fuelPower) {
+	public MaterialBuilder setFuelPower(int fuelPower) {
 		this.fuelPower = fuelPower;
 		return this;
 	}
 
-	public MaterialAdapter setMeltingPoint(int meltingPoint) {
+	public MaterialBuilder setMeltingPoint(int meltingPoint) {
 		this.meltingPoint = meltingPoint;
 		return this;
 	}
 
-	public MaterialAdapter setBlastFurnaceTemp(int blastFurnaceTemp) {
+	public MaterialBuilder setBlastFurnaceTemp(int blastFurnaceTemp) {
 		this.blastFurnaceTemp = blastFurnaceTemp;
 		return this;
 	}
 
-	public MaterialAdapter setBlastFurnaceRequired(boolean blastFurnaceRequired) {
+	public MaterialBuilder setBlastFurnaceRequired(boolean blastFurnaceRequired) {
 		this.blastFurnaceRequired = blastFurnaceRequired;
 		return this;
 	}
 
-	public MaterialAdapter setOreValue(int oreValue) {
+	public MaterialBuilder setOreValue(int oreValue) {
 		this.oreValue = oreValue;
 		return this;
 	}
 
-	public MaterialAdapter setDensityMultiplier(int densityMultiplier) {
+	public MaterialBuilder setDensityMultiplier(int densityMultiplier) {
 		this.densityMultiplier = densityMultiplier;
 		return this;
 	}
 
-	public MaterialAdapter setDensityDivider(int densityDivider) {
+	public MaterialBuilder setDensityDivider(int densityDivider) {
 		this.densityDivider = densityDivider;
 		return this;
 	}
 
-	public MaterialAdapter setExtraData(int extraData) {
+	public MaterialBuilder setExtraData(int extraData) {
 		this.extraData = extraData;
 		return this;
 	}
 
-	public MaterialAdapter addElectrolyzerRecipe(){
+	public MaterialBuilder addElectrolyzerRecipe(){
 		extraData = extraData | 1;
 		return this;
 	}
 	
-	public MaterialAdapter addCentrifugeRecipe(){
+	public MaterialBuilder addCentrifugeRecipe(){
 		extraData = extraData | 2;
 		return this;
 	}
 	
-	public MaterialAdapter setMaterialList(List<MaterialStack> materialList) {
+	public MaterialBuilder setMaterialList(List<MaterialStack> materialList) {
 		this.materialList = materialList;
 		return this;
 	}
 	
-	public MaterialAdapter setMaterialList(MaterialStack ... materials) {
+	public MaterialBuilder setMaterialList(MaterialStack ... materials) {
 		this.materialList = Arrays.asList(materials);
 		return this;
 	}
 
-	public MaterialAdapter setAspects(List<TC_Aspects.TC_AspectStack> aspects) {
+	public MaterialBuilder setAspects(List<TC_Aspects.TC_AspectStack> aspects) {
 		this.aspects = aspects;
 		return this;
 	}
