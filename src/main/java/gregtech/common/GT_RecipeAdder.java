e445fafd3224c8a0f56b6007c2ba1bb1c0be3010
Johannes Gäßler <updrn@student.kit.edu>
2017-05-29 00:13:18 +0200

    Fixed a bug where cicuit configs for distilleries ranged from 0-10 instead of 1-11

--- src/main/java/gregtech/common/GT_RecipeAdder.java
+++ src/main/java/gregtech/common/GT_RecipeAdder.java
@@ -358,7 +358,7 @@ public class GT_RecipeAdder
     @Override
     public boolean addUniversalDistillationRecipe(FluidStack aInput, FluidStack[] aOutputs, ItemStack aOutput2, int aDuration, int aEUt) {
     	for (int i = 0; i < Math.min(aOutputs.length, 11); i++) {
-    		addDistilleryRecipe(GT_Utility.getIntegratedCircuit(i), aInput, aOutputs[i], aDuration * 2, aEUt / 4, false);
+    		addDistilleryRecipe(i + 1, aInput, aOutputs[i], aDuration * 2, aEUt / 4, false);
     	}
         return addDistillationTowerRecipe(aInput, aOutputs, aOutput2, aDuration, aEUt);
     }
