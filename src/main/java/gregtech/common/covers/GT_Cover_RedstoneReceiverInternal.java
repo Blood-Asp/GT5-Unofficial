dc3d19cc9cfa6ab552a7d8de9885edc7fe05fa22
Blood-Asp <bloodasphendrik@gmail.com>
2017-05-31 21:37:26 +0200

    Make internal RS reciever work as machine controller cover

--- src/main/java/gregtech/common/covers/GT_Cover_RedstoneReceiverInternal.java
+++ src/main/java/gregtech/common/covers/GT_Cover_RedstoneReceiverInternal.java
@@ -2,10 +2,18 @@ package gregtech.common.covers;
 
 import gregtech.api.GregTech_API;
 import gregtech.api.interfaces.tileentity.ICoverable;
+import gregtech.api.interfaces.tileentity.IMachineProgress;
 
 public class GT_Cover_RedstoneReceiverInternal
         extends GT_Cover_RedstoneWirelessBase {
     public int doCoverThings(byte aSide, byte aInputRedstone, int aCoverID, int aCoverVariable, ICoverable aTileEntity, long aTimer) {
+        if (aTileEntity instanceof IMachineProgress) {
+            if (getRedstoneInput(aSide, aInputRedstone, aCoverID, aCoverVariable, aTileEntity) >0)
+                ((IMachineProgress) aTileEntity).enableWorking();
+            else
+                ((IMachineProgress) aTileEntity).disableWorking();
+            ((IMachineProgress) aTileEntity).setWorkDataValue(aInputRedstone);
+        }
         return aCoverVariable;
     }
 
