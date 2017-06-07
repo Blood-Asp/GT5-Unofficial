62e467a10f141e953809bcf954e8cca04da52e7b
Techlone <techlone.mc@gmail.com>
2017-06-07 18:19:50 +0500

    cherry pick 541144f4433044a6c0b9612e7cd7f94cf9440095 formating

--- src/main/java/gregtech/api/gui/GT_GUIContainer_MultiMachine.java
+++ src/main/java/gregtech/api/gui/GT_GUIContainer_MultiMachine.java
@@ -31,42 +31,46 @@ public class GT_GUIContainer_MultiMachine extends GT_GUIContainerMetaTile_Machin
 
         if (mContainer != null) {//(mWrench ? 0 : 1) | (mScrewdriver ? 0 : 2) | (mSoftHammer ? 0 : 4) | (mHardHammer ? 0 : 8) | (mSolderingTool ? 0 : 16) | (mCrowbar ? 0 : 32) | (mMachine ? 0 : 64));
             if ((((GT_Container_MultiMachine) mContainer).mDisplayErrorCode & 1) != 0)
-                fontRendererObj.drawString("Pipe is loose.", 10, 16, 16448255);
+                fontRendererObj.drawString(trans("132", "Pipe is loose."), 10, 16, 16448255);
             if ((((GT_Container_MultiMachine) mContainer).mDisplayErrorCode & 2) != 0)
-                fontRendererObj.drawString("Screws are missing.", 10, 24, 16448255);
+                fontRendererObj.drawString(trans("133", "Screws are missing."), 10, 24, 16448255);
             if ((((GT_Container_MultiMachine) mContainer).mDisplayErrorCode & 4) != 0)
-                fontRendererObj.drawString("Something is stuck.", 10, 32, 16448255);
+                fontRendererObj.drawString(trans("134", "Something is stuck."), 10, 32, 16448255);
             if ((((GT_Container_MultiMachine) mContainer).mDisplayErrorCode & 8) != 0)
-                fontRendererObj.drawString("Platings are dented.", 10, 40, 16448255);
+                fontRendererObj.drawString(trans("135", "Platings are dented."), 10, 40, 16448255);
             if ((((GT_Container_MultiMachine) mContainer).mDisplayErrorCode & 16) != 0)
-                fontRendererObj.drawString("Circuitry burned out.", 10, 48, 16448255);
+                fontRendererObj.drawString(trans("136", "Circuitry burned out."), 10, 48, 16448255);
             if ((((GT_Container_MultiMachine) mContainer).mDisplayErrorCode & 32) != 0)
-                fontRendererObj.drawString("That doesn't belong there.", 10, 56, 16448255);
+                fontRendererObj.drawString(trans("137", "That doesn't belong there."), 10, 56, 16448255);
             if ((((GT_Container_MultiMachine) mContainer).mDisplayErrorCode & 64) != 0)
-                fontRendererObj.drawString("Incomplete Structure.", 10, 64, 16448255);
+                fontRendererObj.drawString(trans("138", "Incomplete Structure."), 10, 64, 16448255);
 
             if (((GT_Container_MultiMachine) mContainer).mDisplayErrorCode == 0) {
                 if (((GT_Container_MultiMachine) mContainer).mActive == 0) {
-                    fontRendererObj.drawString("Hit with Soft Mallet", 10, 16, 16448255);
-                    fontRendererObj.drawString("to (re-)start the Machine", 10, 24, 16448255);
-                    fontRendererObj.drawString("if it doesn't start.", 10, 32, 16448255);
+                    fontRendererObj.drawString(trans("139", "Hit with Soft Hammer"), 10, 16, 16448255);
+                    fontRendererObj.drawString(trans("140", "to (re-)start the Machine"), 10, 24, 16448255);
+                    fontRendererObj.drawString(trans("141", "if it doesn't start."), 10, 32, 16448255);
                 } else {
-                    fontRendererObj.drawString("Running perfectly.", 10, 16, 16448255);
+                    fontRendererObj.drawString(trans("142", "Running perfectly."), 10, 16, 16448255);
                 }
-                	int id = mContainer.mTileEntity.getMetaTileID();
-                    if (id == 1157 || id == 1158 || id == 1177 || id == 1178 || id == 1179) {
-                    	ItemStack tItem = mContainer.mTileEntity.getMetaTileEntity().getStackInSlot(1);
-                        if (tItem == null || !GT_Utility.areStacksEqual(tItem, GT_ModHandler.getIC2Item("miningPipe", 1L))) {
-                    		fontRendererObj.drawString("Missing Mining Pipe", 10,((GT_Container_MultiMachine) mContainer).mActive == 0 ? 40 : 24, 16448255);
-                    	}
-                    } else if (id == 1131 || id == 1151 || id == 1152 || id == 1153) {
-                    	ItemStack tItem = mContainer.mTileEntity.getMetaTileEntity().getStackInSlot(1);
-                        if (tItem == null || !(tItem.getItem() == GT_MetaGenerated_Tool_01.INSTANCE && tItem.getItemDamage() >= 170 && tItem.getItemDamage() <= 177)) {
-                            fontRendererObj.drawString("Missing Turbine Rotor", 10, ((GT_Container_MultiMachine) mContainer).mActive == 0 ? 40 : 24, 16448255);
-                    	}
+                int id = mContainer.mTileEntity.getMetaTileID();
+                if (id == 1157 || id == 1158 || id == 1177 || id == 1178 || id == 1179) {
+                    ItemStack tItem = mContainer.mTileEntity.getMetaTileEntity().getStackInSlot(1);
+                    if (tItem == null || !GT_Utility.areStacksEqual(tItem, GT_ModHandler.getIC2Item("miningPipe", 1L))) {
+                        fontRendererObj.drawString(trans("143", "Missing Mining Pipe"), 10, ((GT_Container_MultiMachine) mContainer).mActive == 0 ? 40 : 24, 16448255);
                     }
+                } else if (id == 1131 || id == 1151 || id == 1152 || id == 1153) {
+                    ItemStack tItem = mContainer.mTileEntity.getMetaTileEntity().getStackInSlot(1);
+                    if (tItem == null || !(tItem.getItem() == GT_MetaGenerated_Tool_01.INSTANCE && tItem.getItemDamage() >= 170 && tItem.getItemDamage() <= 177)) {
+                        fontRendererObj.drawString(trans("144", "Missing Turbine Rotor"), 10, ((GT_Container_MultiMachine) mContainer).mActive == 0 ? 40 : 24, 16448255);
+                    }
+                }
             }
         }
+    }
+
+    public String trans(String aKey, String aEnglish) {
+        return GT_LanguageManager.addStringLocalization("Interaction_DESCRIPTION_Index_" + aKey, aEnglish, false);
     }
 
     @Override
