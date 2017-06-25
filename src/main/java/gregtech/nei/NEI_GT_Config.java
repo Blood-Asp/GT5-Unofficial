b7a339d63e350d023c25b80ceb6d7fbb7ffbae1b
Daniel <daniel112092@gmail.com>
2017-06-25 10:42:39 +0200

    Revert "NEI GUI AssemblyLine"

--- src/main/java/gregtech/nei/NEI_GT_Config.java
+++ src/main/java/gregtech/nei/NEI_GT_Config.java
@@ -1,13 +1,11 @@
 package gregtech.nei;
 
 import codechicken.nei.api.IConfigureNEI;
-import cpw.mods.fml.common.FMLCommonHandler;
 import gregtech.api.util.GT_Recipe;
 
 public class NEI_GT_Config
         implements IConfigureNEI {
     public static boolean sIsAdded = true;
-    public static GT_NEI_AssLineHandler ALH;
 
     public void loadConfig() {
         sIsAdded = false;
@@ -16,8 +14,6 @@ public class NEI_GT_Config
                 new GT_NEI_DefaultHandler(tMap);
             }
         }
-        if(FMLCommonHandler.instance().getEffectiveSide().isClient())
-            ALH=new GT_NEI_AssLineHandler(GT_Recipe.GT_Recipe_Map.sAssemblylineVisualRecipes);
         sIsAdded = true;
     }
 
