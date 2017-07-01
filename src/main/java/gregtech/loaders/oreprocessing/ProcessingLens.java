bc68153ef5226c0a1e3a80807610c7f021d3c76e
Dream-Master <dream-master@gmx.net>
2017-07-01 21:32:54 +0200

    remove Flawless Gems because they used for long Rods recipes

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingLens.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingLens.java
@@ -19,17 +19,14 @@ public class ProcessingLens implements gregtech.api.interfaces.IOreRecipeRegistr
     switch (aMaterial.mName) {
             case "Diamond":
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.plate, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial, 1L), (int) Math.max(aMaterial.getMass() / 2L, 1L), 30);
-                GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemFlawless, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 2L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial, 2L), (int) Math.max(aMaterial.getMass() , 1L), 24);
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemExquisite, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 3L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), (int) Math.max(aMaterial.getMass() , 1L), 16);
                 break;
             case "Glass":
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.plate, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial, 1L), (int) Math.max(aMaterial.getMass() / 2L, 1L), 30);
-                GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemFlawless, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 2L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial, 2L), (int) Math.max(aMaterial.getMass() , 1L), 24);
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemExquisite, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 3L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), (int) Math.max(aMaterial.getMass() , 1L), 16);
                 break;
             case "Ruby":
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.plate, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial, 1L), (int) Math.max(aMaterial.getMass() / 2L, 1L), 120);
-                GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemFlawless, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), (int) Math.max(aMaterial.getMass() , 1L), 64);
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemExquisite, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 2L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), (int) Math.max(aMaterial.getMass() , 1L), 30);
                 break;
             case "InfusedFire":
@@ -37,7 +34,6 @@ public class ProcessingLens implements gregtech.api.interfaces.IOreRecipeRegistr
                 break;
             case "Greensapphire":
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.plate, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial, 1L), (int) Math.max(aMaterial.getMass() / 2L, 1L), 120);
-                GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemFlawless, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), (int) Math.max(aMaterial.getMass() , 1L), 64);
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemExquisite, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 2L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), (int) Math.max(aMaterial.getMass() , 1L), 30);
                 break;
             case "InfusedEarth":
@@ -45,7 +41,6 @@ public class ProcessingLens implements gregtech.api.interfaces.IOreRecipeRegistr
                 break;
             default:
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.plate, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dustSmall, aMaterial, 1L), (int) Math.max(aMaterial.getMass() / 2L, 1L), 480);
-                GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemFlawless, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L), (int) Math.max(aMaterial.getMass() , 1L), 120);
                 GT_Values.RA.addLatheRecipe(GT_OreDictUnificator.get(OrePrefixes.gemExquisite, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.lens, aMaterial, 1L), GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 2L), (int) Math.max(aMaterial.getMass() , 1L), 30);
                 GregTech_API.registerCover(aStack, new GT_MultiTexture(new gregtech.api.interfaces.ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[2][0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_LENS, aMaterial.mRGBa, false)}), new gregtech.common.covers.GT_Cover_Lens(aMaterial.mColor.mIndex));
             }
