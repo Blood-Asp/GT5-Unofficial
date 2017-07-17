64957ccd0f51ef283cc561d52a5439c3752c4e71
Antifluxfield <lyj_299792458@163.com>
2017-07-17 16:29:47 +0800

    Update GT_MetaTileEntity_OilDrillBase

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_OilDrillBase.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_OilDrillBase.java
@@ -82,7 +82,8 @@ public abstract class GT_MetaTileEntity_OilDrillBase extends GT_MetaTileEntity_D
         this.mEfficiencyIncrease = 10000;
         //T1 = 24; T2 = 96; T3 = 384
         this.mEUt = 6 * (1 << (getMinTier() << 1));
-        this.mMaxProgresstime = (isPickingPipes ? 80 : 160 * getRangeInChunks() * getRangeInChunks()) / (1 << getMinTier());
+        //160 per chunk in MV
+        this.mMaxProgresstime = (isPickingPipes ? 80 : 640 * getRangeInChunks() * getRangeInChunks()) / (1 << getMinTier());
 
         long voltage = getMaxInputVoltage();
         long overclockEu = V[Math.max(1, GT_Utility.getTier(voltage)) - 1];
