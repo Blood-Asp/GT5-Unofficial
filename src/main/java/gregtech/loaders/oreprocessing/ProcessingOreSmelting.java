02153358674d88d0014af33912b056a044d6e4f4
Johannes Gäßler <updrn@student.kit.edu>
2017-07-24 19:12:42 +0200

    Increased Direct Smelting yield: 5 Nuggets -> 6 Nuggets
    Previously you would only get 1 extra Nugget from Macerating Ore.
    Now you get 3 extra Nuggets.

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingOreSmelting.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingOreSmelting.java
@@ -35,7 +35,7 @@ public class ProcessingOreSmelting implements gregtech.api.interfaces.IOreRecipe
                     	if (aMaterial.mDirectSmelting == aMaterial) {
                     		outputSize = 10;
                     	} else {
-                    		outputSize = 5;
+                    		outputSize = 6;
                     	}
                     	break;
                     case dust:
@@ -58,7 +58,7 @@ public class ProcessingOreSmelting implements gregtech.api.interfaces.IOreRecipe
                     		outputSize = 1;
                     	} else {
                     		outputPrefix = OrePrefixes.nugget;
-                    		outputSize = 5;
+                    		outputSize = 6;
                     	}
                         break;
                     default:
