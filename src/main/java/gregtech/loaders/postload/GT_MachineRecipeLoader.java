4b65784535dfa6c61de61ff0dbe09fc648870c7c
Blood-Asp <bloodasphendrik@gmail.com>
2017-11-25 21:30:22 +0100

    Alternative high tier Lapo Orb Cluster recipe to reduce nessary Lapocrystal amount.

--- src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
+++ src/main/java/gregtech/loaders/postload/GT_MachineRecipeLoader.java
@@ -2255,6 +2255,22 @@ if(Loader.isModLoaded("Railcraft")){
             		Materials.SolderingAlloy.getMolten(2880),
             		GregTech_API.mIC2Classic ? Materials.Water.getFluid(10000) : new FluidStack(FluidRegistry.getFluid("ic2coolant"), 10000)
             }, ItemList.Circuit_Wetwaremainframe.get(1,o), 2000, 300000);
+   
+            GT_Values.RA.addAssemblylineRecipe(ItemList.Energy_LapotronicOrb.get(1, o), 288000, new ItemStack[]{
+            		ItemList.Circuit_Board_Multifiberglass.get(1,o),
+                    GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Europium, 32L),
+                    ItemList.Circuit_Neuroprocessor.get(4, o),
+                    ItemList.Circuit_Parts_Crystal_Chip_Master.get(36L,o),
+                    ItemList.Circuit_Parts_Crystal_Chip_Master.get(36L,o),
+                    ItemList.Circuit_Chip_HPIC.get(64, o),
+                    ItemList.Circuit_Parts_DiodeSMD.get(32, o),
+                    ItemList.Circuit_Parts_CapacitorSMD.get(32, o),
+                    ItemList.Circuit_Parts_ResistorSMD.get(32, o),
+                    ItemList.Circuit_Parts_TransistorSMD.get(32, o),
+                    GT_OreDictUnificator.get(OrePrefixes.wireFine, Materials.Platinum, 64),
+            }, new FluidStack[]{
+                    Materials.SolderingAlloy.getMolten(720)
+            }, ItemList.Energy_LapotronicOrb2.get(1, o), 1000, 80000);
 
         if (GregTech_API.sOPStuff.get(ConfigCategories.Recipes.gregtechrecipes, "EnableZPMandUVBatteries", false)) {
             GT_Values.RA.addAssemblylineRecipe(ItemList.Energy_LapotronicOrb2.get(1, o), 288000, new ItemStack[]{
