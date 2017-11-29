c89a99c36d3f415f89882d8b418f99c3c5d3b974
Antifluxfield <lyj_299792458@163.com>
2017-11-29 10:02:01 +0800

    Fix wrong translation

--- src/main/java/gregtech/nei/GT_NEI_DefaultHandler.java
+++ src/main/java/gregtech/nei/GT_NEI_DefaultHandler.java
@@ -226,7 +226,7 @@ public class GT_NEI_DefaultHandler
 			} else if (tSpecial == -200 && GT_Mod.gregtechproxy.mEnableCleanroom) {
 				drawText(10, 123, trans("160","Needs Cleanroom"), -16777216);
 			} else if (tSpecial == -300) {
-				drawText(10, 123, trans("161","Deprecated Recipe"), -16777216);
+				drawText(10, 123, trans("216","Deprecated Recipe"), -16777216);
 			} else if ((GT_Utility.isStringValid(this.mRecipeMap.mNEISpecialValuePre)) || (GT_Utility.isStringValid(this.mRecipeMap.mNEISpecialValuePost))) {
 				drawText(10, 123, this.mRecipeMap.mNEISpecialValuePre + tSpecial * this.mRecipeMap.mNEISpecialValueMultiplier + this.mRecipeMap.mNEISpecialValuePost, -16777216);
 			}
