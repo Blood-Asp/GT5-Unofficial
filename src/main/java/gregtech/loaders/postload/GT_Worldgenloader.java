d6c5ea32b71132785fccb91b4977dd70a51439d1
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2018-05-06 18:50:05 +0200

    Enabled Reflexion for Galactigreg

--- src/main/java/gregtech/loaders/postload/GT_Worldgenloader.java
+++ src/main/java/gregtech/loaders/postload/GT_Worldgenloader.java
@@ -256,18 +256,18 @@ public class GT_Worldgenloader
             new GT_Worldgen_GT_Ore_Layer("ore.mix.custom." + (i < 10 ? "0" : "") + i, false, 0, 0, 0, 0, 0, false, false, false, Materials._NULL, Materials._NULL, Materials._NULL, Materials._NULL);
         }
         
-        //DO NOT DELETE V THIS V - redundant, removed, GALACTIGREG should load itself when jar is in mods folder!, in case if it is REALLY NEEDED, use this reflection but i highly doubt it...
-        //try {
-        //    Class clazz = Class.forName("bloodasp.galacticgreg.WorldGenGaGT");
-        //    Constructor constructor=clazz.getConstructor();
-        //    Method method=clazz.getMethod("run");
-        //    method.invoke(constructor.newInstance());
-        //    GT_Log.out.println("Started Galactic Greg ore gen code");
-        //    //this function calls Galactic Greg and enables its generation.
-        //}catch (Exception e){
-        //    GT_Log.err.println("Unable to start Galactic Greg ore gen code");
-        //    e.printStackTrace(GT_Log.err);
-        //}
+        //DO NOT DELETE V THIS V - this is needed so that gregtech generates its Ore Layer's first (the ones up there), which can then be transformed into "GT_Worldgen_GT_Ore_Layer_Space". Also Reflexion is slow.
+        try {
+            Class clazz = Class.forName("bloodasp.galacticgreg.WorldGenGaGT");
+            Constructor constructor=clazz.getConstructor();
+            Method method=clazz.getMethod("run");
+            method.invoke(constructor.newInstance());
+            GT_Log.out.println("Started Galactic Greg ore gen code");
+            //this function calls Galactic Greg and enables its generation.
+        }catch (Exception e){
+            GT_Log.err.println("Unable to start Galactic Greg ore gen code");
+            e.printStackTrace(GT_Log.err);
+        }
         //DO NOT DELETE ^ THIS ^
         
         /*if (GregTech_API.mImmersiveEngineering && GT_Mod.gregtechproxy.mImmersiveEngineeringRecipes) {
