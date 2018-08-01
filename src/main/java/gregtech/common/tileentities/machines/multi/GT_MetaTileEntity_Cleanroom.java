1627d18647a1b999a7b892c2f36063b6c78f832f
Dream-Master <dream-master@gmx.net>
2018-08-01 19:57:19 +0200

    cleanroom not working in LV #3355 https://github.com/GTNewHorizons/NewHorizons/issues/3355

--- src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_Cleanroom.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/GT_MetaTileEntity_Cleanroom.java
@@ -42,10 +42,13 @@ public class GT_MetaTileEntity_Cleanroom extends GT_MetaTileEntity_MultiBlockBas
                 "Controller (Top center)",
                 "Top besides contoller and edges: Filter Casings",
                 "1 Reinforced Door (keep closed for 100% efficency)",
-				"2x LV or 1MV Energy Hatch, 1x Maintainance Hatch",
+				"1x LV or 1x MV Energy Hatch, 1x Maintainance Hatch",
                 "Up to 10 Machine Hull Item & Energy transfer through walls",
                 "Remaining Blocks: Plascrete, 20 min",
-				"Consumes 40 EU/t when first turned on and 4 EU/t once at 100% efficiency"};
+				"Consumes 40 EU/t when first turned on and 4 EU/t once at 100% efficiency",
+				"An energy hatch accepts up to 2A, so you can use 2A LV or 1A MV",
+				"2 LV batteries + 1 LV generator or 1 MV generator",
+				"Make sure your Energy Hatch matches!"};
     }
 
 	@Override
