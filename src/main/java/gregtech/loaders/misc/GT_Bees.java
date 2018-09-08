bb1993dd4e9bd6e57466c93d6ce088b9fe8f691f
Dream-Master <dream-master@gmx.net>
2018-09-08 19:29:46 +0200

    fix derp. Will add drops later

--- src/main/java/gregtech/loaders/misc/GT_Bees.java
+++ src/main/java/gregtech/loaders/misc/GT_Bees.java
@@ -3,17 +3,14 @@ package gregtech.loaders.misc;
 import cpw.mods.fml.common.Loader;
 import gregtech.GT_Mod;
 import gregtech.common.items.ItemComb;
-import gregtech.common.items.ItemDrop;
 
 public class GT_Bees {
 
     public static ItemComb combs;
-    public static ItemDrop drops;
 
     public GT_Bees() {
         if (Loader.isModLoaded("Forestry") && GT_Mod.gregtechproxy.mGTBees) {
             combs = new ItemComb();
-            drops = new ItemDrop();
             combs.initCombsRecipes();
             GT_BeeDefinition.initBees();            
         }
