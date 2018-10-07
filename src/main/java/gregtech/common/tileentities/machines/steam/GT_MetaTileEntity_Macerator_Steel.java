0e5d1f7662cff85742aaaffe717dc15942a2ce2d
Dream-Master <dream-master@gmx.net>
2018-10-07 20:41:22 +0200

    cherry pick b46a466878c07f8343894899304dce605b6b1d44 [b46a466] Fixed bug in macerator recipes. (#1468)

--- src/main/java/gregtech/common/tileentities/machines/steam/GT_MetaTileEntity_Macerator_Steel.java
+++ src/main/java/gregtech/common/tileentities/machines/steam/GT_MetaTileEntity_Macerator_Steel.java
@@ -61,11 +61,11 @@ public class GT_MetaTileEntity_Macerator_Steel extends GT_MetaTileEntity_BasicMa
 		
 //        if (!tRecipe.isRecipeInputEqual(true, new FluidStack[]{getFillableStack()}, getAllInputs()))
 //                    return FOUND_RECIPE_BUT_DID_NOT_MEET_REQUIREMENTS;
-
+        if (!tRecipe.isRecipeInputEqual(true, new FluidStack[]{getFillableStack()}, getAllInputs()))
+            return FOUND_RECIPE_BUT_DID_NOT_MEET_REQUIREMENTS;
         if (tRecipe.getOutput(0) != null) mOutputItems[0] = tRecipe.getOutput(0);
         this.mEUt = (2 * tRecipe.mEUt);
         this.mMaxProgresstime = tRecipe.mDuration;
-        getInputAt(0).stackSize -= tRecipe.mInputs[0].stackSize;
         return FOUND_AND_SUCCESSFULLY_USED_RECIPE;
     }
 
