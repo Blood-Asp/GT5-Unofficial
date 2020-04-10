93af178db9c7d457a22a39cb93229e40362ffd34
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2020-04-10 17:58:28 +0200

    Added all Ores to Voidminer in DeepDark + removed Infinity Ore
    Signed-off-by: bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>

--- src/main/java/gregtech/api/util/GT_BartWorks_Compat.java
+++ src/main/java/gregtech/api/util/GT_BartWorks_Compat.java
@@ -1,9 +1,12 @@
 package gregtech.api.util;
 
+import com.github.bartimaeusnek.bartworks.API.VoidMinerDropAdder;
 import com.github.bartimaeusnek.bartworks.API.WerkstoffAPI;
 import com.github.bartimaeusnek.bartworks.system.material.Werkstoff;
 import gregtech.api.enums.Materials;
+import gregtech.api.interfaces.ISubTagContainer;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 
 public class GT_BartWorks_Compat {
@@ -28,6 +31,14 @@ public class GT_BartWorks_Compat {
 
     public static Materials getBartWorksMaterialByID(int id) {
         return Optional.ofNullable(Optional.ofNullable(Werkstoff.werkstoffHashMap.get((short) id)).orElse(Werkstoff.default_null_Werkstoff).getBridgeMaterial()).orElse(Materials._NULL);
+    }
+
+    public static void addVoidMinerDropsToDimension(int dimID, ISubTagContainer material, float chance){
+        try {
+            VoidMinerDropAdder.addDropsToDim(dimID, material, chance);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 }
