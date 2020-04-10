5e3ca4304eaa66bc0250878d8d1bc71022afedf8
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2020-04-10 15:41:32 +0200

    Removed T5 & T6 Miners + Added BW Compat for easier use of BW Materials in code + Removed DD Veins
    Signed-off-by: bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>

---
+++ src/main/java/gregtech/api/util/GT_BartWorks_Compat.java
@@ -0,0 +1,33 @@
+package gregtech.api.util;
+
+import com.github.bartimaeusnek.bartworks.API.WerkstoffAPI;
+import com.github.bartimaeusnek.bartworks.system.material.Werkstoff;
+import gregtech.api.enums.Materials;
+
+import java.util.Optional;
+
+public class GT_BartWorks_Compat {
+
+    public static Materials getBartWorksMaterialByVarName(String name) {
+        Materials materials = Materials._NULL;
+        try {
+            materials = new WerkstoffAPI().getWerkstoff(name).getBridgeMaterial();
+        } catch (NoSuchFieldException | IllegalAccessException exception) {
+            exception.printStackTrace();
+        }
+        return materials;
+    }
+
+    public static Materials getBartWorksMaterialByIGNName(String name) {
+        Optional<Werkstoff> material = Werkstoff.werkstoffHashSet.stream().filter(e -> e.getDefaultName().equals(name)).findFirst();
+        if (material.isPresent()) {
+            return material.get().getBridgeMaterial();
+        }
+        return Materials._NULL;
+    }
+
+    public static Materials getBartWorksMaterialByID(int id) {
+        return Optional.ofNullable(Optional.ofNullable(Werkstoff.werkstoffHashMap.get((short) id)).orElse(Werkstoff.default_null_Werkstoff).getBridgeMaterial()).orElse(Materials._NULL);
+    }
+
+}
