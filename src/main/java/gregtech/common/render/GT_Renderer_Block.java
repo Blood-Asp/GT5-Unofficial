82f7e5c86e141c5206ac087cc2f7c45229d69df4
Léa Gris <lea.gris@noiraude.net>
2021-01-25 18:40:26 +0100

    fix(gregtech): tile itemblock bottom side rendering (#421)
    Bottom side of itemblock tiles was not rendered.

--- src/main/java/gregtech/common/render/GT_Renderer_Block.java
+++ src/main/java/gregtech/common/render/GT_Renderer_Block.java
@@ -419,10 +419,12 @@ public class GT_Renderer_Block implements ISimpleBlockRenderingHandler {
     }
 
     public static void renderNegativeYFacing(IBlockAccess aWorld, RenderBlocks aRenderer, Block aBlock, int aX, int aY, int aZ, ITexture[] aIcon, boolean aFullBlock) {
-        if (aWorld == null) return;
-        if ((aFullBlock) && (!aBlock.shouldSideBeRendered(aWorld, aX, aY - 1, aZ, 0))) return;
-        Tessellator.instance.setBrightness(aBlock.getMixedBrightnessForBlock(aWorld, aX, aFullBlock ? aY - 1 : aY, aZ));
-
+        if (aWorld != null) {
+            if ((aFullBlock) && (!aBlock.shouldSideBeRendered(aWorld, aX, aY - 1, aZ, 0))) {
+                return;
+            }
+            Tessellator.instance.setBrightness(aBlock.getMixedBrightnessForBlock(aWorld, aX, aFullBlock ? aY - 1 : aY, aZ));
+        }
         if (aIcon != null) {
             for (ITexture iTexture : aIcon) {
                 if (iTexture != null) {
