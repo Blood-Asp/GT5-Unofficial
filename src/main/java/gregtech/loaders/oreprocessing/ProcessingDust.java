cf02b624992af565f50576dd9f029473fb288c06
Prometheus0000 <prometheus0000000@gmail.com>
2021-05-06 16:43:09 -0400

    Removes autogeneration of tiny dust to nugget smelting recipe for non-EBF materials
    Does anyone use this?

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingDust.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingDust.java
@@ -222,7 +222,7 @@ public class ProcessingDust implements gregtech.api.interfaces.IOreRecipeRegistr
                             //GT_Values.RA.addBlastRecipe(GT_Utility.copyAmount(9L, aStack), ItemList.Circuit_Integrated.getWithDamage(0L, 9L), null, null, aMaterial.mBlastFurnaceTemp > 1750 ? GT_OreDictUnificator.get(OrePrefixes.ingotHot, aMaterial.mSmeltInto, GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), 1L) : GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), null, (int) Math.max(aMaterial.getMass() / 40L, 1L) * aMaterial.mBlastFurnaceTemp, 120, aMaterial.mBlastFurnaceTemp);
                         GT_ModHandler.removeFurnaceSmelting(aStack);
                     } else {
-                        GT_ModHandler.addSmeltingRecipe(GT_Utility.copyAmount(1L, aStack), GT_OreDictUnificator.get(OrePrefixes.nugget, aMaterial.mSmeltInto, 1L));
+                        //GT_ModHandler.addSmeltingRecipe(GT_Utility.copyAmount(1L, aStack), GT_OreDictUnificator.get(OrePrefixes.nugget, aMaterial.mSmeltInto, 1L));
                         //GT_ModHandler.addAlloySmelterRecipe(GT_Utility.copyAmount(9L, aStack), ItemList.Shape_Mold_Ingot.get(0L), GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial.mSmeltInto, 1L), 130, 3, true);
                     }
                 }
