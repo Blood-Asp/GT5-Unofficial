6523c1f749550e16ff03668b10cbe9c11cebffeb
Léa Gris <lea.gris@noiraude.net>
2021-05-07 12:32:37 +0200

    feat(render): active steam extractor front glow place-holder blank to support glow texture in resourcepack

--- src/main/java/gregtech/common/tileentities/machines/steam/GT_MetaTileEntity_Extractor_Steel.java
+++ src/main/java/gregtech/common/tileentities/machines/steam/GT_MetaTileEntity_Extractor_Steel.java
@@ -1,16 +1,26 @@
 package gregtech.common.tileentities.machines.steam;
 
 import gregtech.api.GregTech_API;
-import gregtech.api.enums.Textures;
 import gregtech.api.gui.GT_GUIContainer_BasicMachine;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicMachine_Steel;
+import gregtech.api.objects.GT_RenderedGlowTexture;
 import gregtech.api.objects.GT_RenderedTexture;
 import gregtech.api.util.GT_Recipe;
 import gregtech.api.util.GT_Utility;
 import net.minecraft.entity.player.InventoryPlayer;
+
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_BOTTOM_STEAM_EXTRACTOR;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_BOTTOM_STEAM_EXTRACTOR_ACTIVE;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_FRONT_STEAM_EXTRACTOR;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_FRONT_STEAM_EXTRACTOR_ACTIVE;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_FRONT_STEAM_EXTRACTOR_ACTIVE_GLOW;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_SIDE_STEAM_EXTRACTOR;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_SIDE_STEAM_EXTRACTOR_ACTIVE;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_TOP_STEAM_EXTRACTOR;
+import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_TOP_STEAM_EXTRACTOR_ACTIVE;
 
 public class GT_MetaTileEntity_Extractor_Steel extends GT_MetaTileEntity_BasicMachine_Steel {
     public GT_MetaTileEntity_Extractor_Steel(int aID, String aName, String aNameRegional) {
@@ -25,14 +35,17 @@ public class GT_MetaTileEntity_Extractor_Steel extends GT_MetaTileEntity_BasicMa
         super(aName, aDescription, aTextures, 1, 1, false);
     }
 
+    @Override
     public Object getClientGUI(int aID, InventoryPlayer aPlayerInventory, IGregTechTileEntity aBaseMetaTileEntity) {
         return new GT_GUIContainer_BasicMachine(aPlayerInventory, aBaseMetaTileEntity, getLocalName(), "SteelExtractor.png", GT_Recipe.GT_Recipe_Map.sExtractorRecipes.mUnlocalizedName);
     }
 
+    @Override
     public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
         return new GT_MetaTileEntity_Extractor_Steel(this.mName, this.mDescriptionArray, this.mTextures);
     }
 
+    @Override
     public int checkRecipe() {
         GT_Recipe tRecipe = GT_Recipe.GT_Recipe_Map.sExtractorRecipes.findRecipe(getBaseMetaTileEntity(), false, gregtech.api.enums.GT_Values.V[2], null, getAllInputs());
         if ((tRecipe != null) && (canOutput(tRecipe.mOutputs)) && (tRecipe.isRecipeInputEqual(true, null, getAllInputs()))) {
@@ -44,46 +57,73 @@ public class GT_MetaTileEntity_Extractor_Steel extends GT_MetaTileEntity_BasicMa
         return 0;
     }
 
+    @Override
     public void startSoundLoop(byte aIndex, double aX, double aY, double aZ) {
         super.startSoundLoop(aIndex, aX, aY, aZ);
         if (aIndex == 1) {
-            GT_Utility.doSoundAtClient((String) GregTech_API.sSoundList.get(200), 10, 1.0F, aX, aY, aZ);
+            GT_Utility.doSoundAtClient(GregTech_API.sSoundList.get(200), 10, 1.0F, aX, aY, aZ);
         }
     }
 
+    @Override
     public void startProcess() {
         sendLoopStart((byte) 1);
     }
 
+    @Override
     public ITexture[] getSideFacingActive(byte aColor) {
-        return new ITexture[]{super.getSideFacingActive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_SIDE_STEAM_EXTRACTOR_ACTIVE)};
+        return new ITexture[]{
+                super.getSideFacingActive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_SIDE_STEAM_EXTRACTOR_ACTIVE)};
     }
 
+    @Override
     public ITexture[] getSideFacingInactive(byte aColor) {
-        return new ITexture[]{super.getSideFacingInactive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_SIDE_STEAM_EXTRACTOR)};
+        return new ITexture[]{
+                super.getSideFacingInactive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_SIDE_STEAM_EXTRACTOR)};
     }
 
+    @Override
     public ITexture[] getFrontFacingActive(byte aColor) {
-        return new ITexture[]{super.getFrontFacingActive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_FRONT_STEAM_EXTRACTOR_ACTIVE)};
+        return new ITexture[]{
+                super.getFrontFacingActive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_FRONT_STEAM_EXTRACTOR_ACTIVE),
+                new GT_RenderedGlowTexture(OVERLAY_FRONT_STEAM_EXTRACTOR_ACTIVE_GLOW)};
     }
 
+    @Override
     public ITexture[] getFrontFacingInactive(byte aColor) {
-        return new ITexture[]{super.getFrontFacingInactive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_FRONT_STEAM_EXTRACTOR)};
+        return new ITexture[]{
+                super.getFrontFacingInactive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_FRONT_STEAM_EXTRACTOR)};
     }
 
+    @Override
     public ITexture[] getTopFacingActive(byte aColor) {
-        return new ITexture[]{super.getTopFacingActive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_TOP_STEAM_EXTRACTOR_ACTIVE)};
+        return new ITexture[]{
+                super.getTopFacingActive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_TOP_STEAM_EXTRACTOR_ACTIVE)};
     }
 
+    @Override
     public ITexture[] getTopFacingInactive(byte aColor) {
-        return new ITexture[]{super.getTopFacingInactive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_TOP_STEAM_EXTRACTOR)};
+        return new ITexture[]{
+                super.getTopFacingInactive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_TOP_STEAM_EXTRACTOR)};
     }
 
+    @Override
     public ITexture[] getBottomFacingActive(byte aColor) {
-        return new ITexture[]{super.getBottomFacingActive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_BOTTOM_STEAM_EXTRACTOR_ACTIVE)};
+        return new ITexture[]{
+                super.getBottomFacingActive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_BOTTOM_STEAM_EXTRACTOR_ACTIVE)};
     }
 
+    @Override
     public ITexture[] getBottomFacingInactive(byte aColor) {
-        return new ITexture[]{super.getBottomFacingInactive(aColor)[0], new GT_RenderedTexture(Textures.BlockIcons.OVERLAY_BOTTOM_STEAM_EXTRACTOR)};
+        return new ITexture[]{
+                super.getBottomFacingInactive(aColor)[0],
+                new GT_RenderedTexture(OVERLAY_BOTTOM_STEAM_EXTRACTOR)};
     }
 }
