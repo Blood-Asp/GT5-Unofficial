1c10a3c9d8ab37d65d7b2278ced0198f8f8a9888
D-Cysteine <54219287+D-Cysteine@users.noreply.github.com>
2021-07-26 10:27:47 -0600

    Delete no longer accurate comment, and disable with if rather than commenting out

--- src/main/java/gregtech/loaders/materialprocessing/ProcessingConfig.java
+++ src/main/java/gregtech/loaders/materialprocessing/ProcessingConfig.java
@@ -15,15 +15,13 @@ public class ProcessingConfig implements gregtech.api.interfaces.IMaterialHandle
     @Override
     public void onMaterialsInit() {
         /** This is just left here as an example of how to add new materials. **/
-        // If you uncomment this, these custom materials will get added to the Deep Dark void miner ore list and cause bugged ores to be generated!
-        // See: com.dreammaster.bartworksHandler.VoidMinerLoader
-        /*
-        int i = 0;
-        for (int j = GregTech_API.sMaterialProperties.get("general", "AmountOfCustomMaterialSlots", 16); i < j; i++) {
-            String aID = (i < 10 ? "0" : "") + i;
-            new Materials(-1, TextureSet.SET_METALLIC, 1.0F, 0, 0, 0, 255, 255, 255, 0, "CustomMat" + aID, "CustomMat" + aID, 0, 0, 0, 0, false, false, 1, 1, 1, Dyes._NULL, "custom", true, aID);
+        if (false) {
+            int i = 0;
+            for (int j = GregTech_API.sMaterialProperties.get("general", "AmountOfCustomMaterialSlots", 16); i < j; i++) {
+                String aID = (i < 10 ? "0" : "") + i;
+                new Materials(-1, TextureSet.SET_METALLIC, 1.0F, 0, 0, 0, 255, 255, 255, 0, "CustomMat" + aID, "CustomMat" + aID, 0, 0, 0, 0, false, false, 1, 1, 1, Dyes._NULL, "custom", true, aID);
+            }
         }
-        */
     }
 
     @Override
