35937ca89911c2b29f0a6027963c7439762045e7
TimeConqueror <timeconqueror999@gmail.com>
2021-08-03 23:13:02 +0300

    Removed ArrayIterator and NonNullIterator, because they're redundant

--- src/main/java/gregtech/api/util/extensions/IteratorExt.java
+++ src/main/java/gregtech/api/util/extensions/IteratorExt.java
@@ -1,8 +1,6 @@
 package gregtech.api.util.extensions;
 
-import gregtech.api.objects.iterators.ArrayIterator;
 import gregtech.api.objects.iterators.MergedIterator;
-import gregtech.api.objects.iterators.NonNullIterator;
 
 import java.util.Iterator;
 
@@ -10,14 +8,5 @@ public class IteratorExt {
     @SafeVarargs
     public static <T> Iterator<T> merge(Iterator<T>... iterators) {
         return new MergedIterator<>(iterators);
-    }
-
-    public static <T> Iterator<T> withoutNulls(Iterator<T> iterator) {
-        return new NonNullIterator<>(iterator);
-    }
-
-    @SafeVarargs
-    public static <T> ArrayIterator<T> ofArray(T... items) {
-        return new ArrayIterator<>(items);
     }
 }
