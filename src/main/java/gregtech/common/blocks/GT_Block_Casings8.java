8c17978d8cccd9f15e6230aba4f22e157261cde3
kuba6000 <kuba.123123.6000@gmail.com>
2021-09-05 02:19:08 +0200

    New casing added

--- src/main/java/gregtech/common/blocks/GT_Block_Casings8.java
+++ src/main/java/gregtech/common/blocks/GT_Block_Casings8.java
@@ -14,7 +14,7 @@ public class GT_Block_Casings8 extends GT_Block_Casings_Abstract {
     //WATCH OUT FOR TEXTURE ID's
     public GT_Block_Casings8() {
         super(GT_Item_Casings8.class, "gt.blockcasings8", GT_Material_Casings.INSTANCE);
-        for (int i = 0; i < 5; i = (i + 1)) {
+        for (int i = 0; i < 6; i = (i + 1)) {
             Textures.BlockIcons.casingTexturePages[1][i+48] = TextureFactory.of(this, i);
         }
         GT_LanguageManager.addStringLocalization(getUnlocalizedName() + ".0.name", "Chemically Inert Machine Casing");
@@ -22,12 +22,14 @@ public class GT_Block_Casings8 extends GT_Block_Casings_Abstract {
         GT_LanguageManager.addStringLocalization(getUnlocalizedName() + ".2.name", "Mining Neutronium Casing");
         GT_LanguageManager.addStringLocalization(getUnlocalizedName() + ".3.name", "Mining Black Plutonium Casing");
         GT_LanguageManager.addStringLocalization(getUnlocalizedName() + ".4.name", "Extreme Engine Intake Casing");
+        GT_LanguageManager.addStringLocalization(getUnlocalizedName() + ".5.name", "Europium Reinforced Radiation Proof Machine Casing");
 
         ItemList.Casing_Chemically_Inert.set(new ItemStack(this, 1, 0));
         ItemList.Casing_Pipe_Polytetrafluoroethylene.set(new ItemStack(this, 1, 1));
         ItemList.Casing_MiningNeutronium.set(new ItemStack(this, 1, 2));
         ItemList.Casing_MiningBlackPlutonium.set(new ItemStack(this, 1, 3));
         ItemList.Casing_ExtremeEngineIntake.set(new ItemStack(this, 1, 4));
+        ItemList.Casing_AdvancedRadiationProof.set(new ItemStack(this, 1, 5));
     }
 
     @Override
@@ -44,6 +46,8 @@ public class GT_Block_Casings8 extends GT_Block_Casings_Abstract {
             return Textures.BlockIcons.MACHINE_CASING_MINING_BLACKPLUTONIUM.getIcon();
         case 4:
             return Textures.BlockIcons.MACHINE_CASING_EXTREME_ENGINE_INTAKE.getIcon();//changed color in a terrible way
+        case 5:
+            return Textures.BlockIcons.MACHINE_CASING_ADVANCEDRADIATIONPROOF.getIcon();
         }
         return Textures.BlockIcons.MACHINE_CASING_ROBUST_TUNGSTENSTEEL.getIcon();
     }
