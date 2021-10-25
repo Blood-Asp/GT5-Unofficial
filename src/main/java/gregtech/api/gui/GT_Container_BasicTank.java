fde09b245b4336fa5a7b396d0432d7cc87b06efc
Glease <4586901+Glease@users.noreply.github.com>
2021-10-25 10:19:37 +0800

    Fix quantum tanks fluid amount flickering
    par2 Might be negative under certain circumstance, causing the mContent counter to be temporarily negative.

--- src/main/java/gregtech/api/gui/GT_Container_BasicTank.java
+++ src/main/java/gregtech/api/gui/GT_Container_BasicTank.java
@@ -223,10 +223,10 @@ public class GT_Container_BasicTank extends GT_ContainerMetaTile_Machine {
         super.updateProgressBar(par1, par2);
         switch (par1) {
             case 100:
-                mContent = mContent & -65536 | par2;
+                mContent = mContent & 0xffff0000 | par2 & 0x0000ffff;
                 break;
             case 101:
-                mContent = mContent & 65535 | par2 << 16;
+                mContent = mContent & 0xffff | par2 << 16;
                 break;
         }
     }
