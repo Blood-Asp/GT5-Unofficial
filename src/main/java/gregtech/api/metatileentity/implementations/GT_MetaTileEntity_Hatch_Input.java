76d4a31d0d0fad80796359b24c61f1c2a54d8699
Martin Robertz <dream-master@gmx.net>
2021-11-07 16:04:58 +0100

    fix derp
    (cherry picked from commit d602218c160a7c2e2d4e84ae5ba4a32c829a2ca4)

--- src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_Hatch_Input.java
+++ src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_Hatch_Input.java
@@ -21,7 +21,7 @@ public class GT_MetaTileEntity_Hatch_Input extends GT_MetaTileEntity_Hatch {
     public GT_MetaTileEntity_Hatch_Input(int aID, String aName, String aNameRegional, int aTier) {
         super(aID, aName, aNameRegional, aTier, 3, new String[]{
                 "Fluid Input for Multiblocks",
-                "Capacity: " + GT_Utility.formatNumbers(8000*(1>>aTier)) + "L"});
+                "Capacity: " + GT_Utility.formatNumbers(8000*(1<<aTier)) + "L"});
     }
 
     public GT_MetaTileEntity_Hatch_Input(String aName, int aTier, String aDescription, ITexture[][][] aTextures) {
