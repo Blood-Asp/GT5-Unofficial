ea9df088740cf44d8f77275b17f519127750bf4f
Glease <4586901+Glease@users.noreply.github.com>
2021-11-16 06:46:29 +0800

    convert ItemFilter to use NBTTagCompound storage
    Signed-off-by: Glease <4586901+Glease@users.noreply.github.com>

--- src/main/java/gregtech/api/util/ISerializableObject.java
+++ src/main/java/gregtech/api/util/ISerializableObject.java
@@ -3,10 +3,16 @@ package gregtech.api.util;
 import com.google.common.io.ByteArrayDataInput;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTSizeTracker;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagInt;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 
 /**
  * We could well have used {@link java.io.Serializable}, but that's too slow and should generally be avoided
@@ -36,6 +42,41 @@ public interface ISerializableObject {
     // the NBT is an unfortunate piece of tech. everything uses it but its API is not as efficient as could be
     @Nonnull
     ISerializableObject readFromPacket(ByteArrayDataInput aBuf, EntityPlayerMP aPlayer);
+
+    /**
+     * Reverse engineered and adapted {@link cpw.mods.fml.common.network.ByteBufUtils#readTag(ByteBuf)}
+     * Given buffer must contain a serialized NBTTagCompound in minecraft encoding
+     */
+    static NBTTagCompound readCompoundTagFromGreggyByteBuf(ByteArrayDataInput aBuf) {
+        short size = aBuf.readShort();
+        if (size < 0)
+            return null;
+        else  {
+            byte[] buf = new byte[size]; // this is shit, how many copies have we been doing?
+            aBuf.readFully(buf);
+            try {
+                return CompressedStreamTools.func_152457_a(buf, new NBTSizeTracker(2097152L));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Reverse engineered and adapted {@link cpw.mods.fml.common.network.ByteBufUtils#readItemStack(ByteBuf)}
+     * Given buffer must contain a serialized ItemStack in minecraft encoding
+     */
+    static ItemStack readItemStackFromGreggyByteBuf(ByteArrayDataInput aBuf) {
+        ItemStack stack = null;
+        short id = aBuf.readShort();
+        if (id >= 0) {
+            byte size = aBuf.readByte();
+            short meta = aBuf.readShort();
+            stack = new ItemStack(Item.getItemById(id), size, meta);
+            stack.stackTagCompound = readCompoundTagFromGreggyByteBuf(aBuf);
+        }
+        return stack;
+    }
 
     final class LegacyCoverData implements ISerializableObject {
         private int mData;
