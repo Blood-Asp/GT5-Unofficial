d5282de1c6cd6d9a0891e221e56f951a72549cf0
Bass <tudurap.com@gmail.com>
2021-12-02 13:43:38 +0000

    Revert "add 4 slots to Autoclave LV is 1 Slot MV is 2 Slots HV is 3 Slots EV is 4 Slots remove Silicon ebf recipe added new Sio2 recipes"
    This reverts commit 9b127d5ab0157bacdb051f55eb2c41908c6df1d8.

--- src/main/java/gregtech/common/GT_RecipeAdder.java
+++ src/main/java/gregtech/common/GT_RecipeAdder.java
@@ -1108,20 +1108,6 @@ public class GT_RecipeAdder implements IGT_RecipeAdder {
         GT_Recipe.GT_Recipe_Map.sAutoclaveRecipes.addRecipe(true, new ItemStack[]{aInput, aCircuit}, new ItemStack[]{aOutput}, null, new int[]{aChance}, new FluidStack[]{aFluid}, null, aDuration, aEUt, aCleanroom ? -100 : 0);
         return true;
     }
-    @Override
-    public boolean addAutoclave4Recipe(ItemStack aInput, ItemStack aCircuit, FluidStack aFluidIn, FluidStack aFluidOut, ItemStack[] aOutputs, int[] aChances, int aDuration, int aEUt, boolean aCleanroom) {
-        if ((aInput == null) || (aFluidIn == null) || (aOutputs == null)) {
-            return false;
-        }
-        if ((aDuration = GregTech_API.sRecipeFile.get("autoclave", aInput, aDuration)) <= 0) {
-            return false;
-        }
-        if (!GT_Mod.gregtechproxy.mEnableCleanroom){
-            aCleanroom = false;
-        }
-        GT_Recipe.GT_Recipe_Map.sAutoclaveRecipes.addRecipe(true, new ItemStack[] {aInput, aCircuit} , aOutputs, null, aChances, new FluidStack[]{aFluidIn}, new FluidStack[]{aFluidOut}, aDuration, aEUt, aCleanroom ? -200 : 0);
-        return true;
-    }
 
     @Override
     public boolean addMixerRecipe(ItemStack aInput1, ItemStack aInput2, ItemStack aInput3, ItemStack aInput4, FluidStack aFluidInput, FluidStack aFluidOutput, ItemStack aOutput, int aDuration, int aEUt) {
